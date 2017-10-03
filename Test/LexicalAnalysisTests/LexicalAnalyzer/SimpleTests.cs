969f4edb04c9f9c73b83da41ea3fda9bea1f2d24
Karoly Ozsvart <karoly.ozsvart@dgital.com>
2017-10-03 20:53:20 +0200

    Added test case While

--- Test/LexicalAnalysisTests/LexicalAnalyzer/SimpleTests.cs
+++ Test/LexicalAnalysisTests/LexicalAnalyzer/SimpleTests.cs
@@ -200,6 +200,39 @@ namespace LexicalAnalysisTests.LexicalAnalyzer
             SymbolTableTester.SimpleSymbolTableEntry(subTable.Entries[0], "i", SingleEntryType.Egesz, 2);
         }
 
+        [Test]
+        public void While()
+        {
+            const string code = "program_kezd\n" +
+                                "ciklus_amíg hamis\n" +
+                                "\tkilép\n" +
+                                "ciklus_vége\n" +
+                                "program_vége";
+
+            LexicalAnalyzerResult result = new LexicalAnalysis.LexicalAnalyzer().Analyze(code);
+
+            TokenTester tt = new TokenTester(result);
+
+            tt.ExpectStart();
+            tt.NewLine();
+
+            tt.ExpectKeyword("ciklus_amíg");
+            tt.ExpectLogikaiLiteral("hamis");
+            tt.NewLine();
+
+            tt.ExpectKeyword("kilép");
+            tt.NewLine();
+
+            tt.ExpectKeyword("ciklus_vége");
+            tt.NewLine();
+
+            tt.ExpectEnd();
+            tt.ExpectNoMore();
+
+            // Symbol table
+            Assert.That(result.SymbolTable.IsEmpty);
+        }
+
         [TestCaseSource(nameof(SimpleTypeNames))]
         public void SimpleTypeDefinition(string type)
         {
