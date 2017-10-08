3baf1bb27d95879a38831ef1e1490d60d0d4156b
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-10-08 21:36:37 +0200

    Added LoadTests

--- Test/LexicalAnalysisTests/LexicalAnalyzer/LoadTests.cs
+++ Test/LexicalAnalysisTests/LexicalAnalyzer/LoadTests.cs
@@ -1,14 +1,23 @@
-﻿using NUnit.Framework;
+﻿using LexicalAnalysis;
+using LexicalAnalysis.Tokens;
+using NUnit.Framework;
+using System.Linq;
 
 namespace LexicalAnalysisTests.LexicalAnalyzer
 {
     [TestFixture]
     public sealed class LoadTests
     {
-        [Test, Ignore("Not done yet")]
-        public void LoadTest()
+        [Test, Timeout(1000)]
+        public void LoadTest1K() => DoLoadTest(Resources.LoadTests.loadtest1k);
+
+        [Test, Timeout(1000)]
+        public void LoadTest10K() => DoLoadTest(Resources.LoadTests.loadtest10k);
+
+        private static void DoLoadTest(string code)
         {
-            
+            LexicalAnalyzerResult result = new LexicalAnalysis.LexicalAnalyzer().Analyze(code);
+            Assert.That(result.Tokens.Any(t => t is ErrorToken), Is.False);
         }
     }
 }
