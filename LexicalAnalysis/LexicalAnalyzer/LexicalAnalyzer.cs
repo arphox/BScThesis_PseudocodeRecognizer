71155cb2df861f3d4b2ad27eaad72018de4e77cb
Ozsvárt Károly <ozsvart.karoly@gmail.com>
2017-10-10 13:52:32 +0200

    Moved non whitespace recognization

--- LexicalAnalysis/LexicalAnalyzer/LexicalAnalyzer.cs
+++ LexicalAnalysis/LexicalAnalyzer/LexicalAnalyzer.cs
@@ -160,7 +160,7 @@ namespace LexicalAnalysis.LexicalAnalyzer
             int _lastCorrectLength = -1; // last correctly recognised lexical element's length
             string _currentSubstring = "";
 
-            RecognizeNonWhitespace(ref _currentSubstring, ref _lastCorrectCode, ref _lastCorrectLength, _input, _inputIndexer);
+            NonWhitespaceRecognizer.RecognizeNonWhitespace(ref _currentSubstring, ref _lastCorrectCode, ref _lastCorrectLength, _input, _inputIndexer);
 
             if (_lastCorrectCode == LexicalElementCodeDictionary.ErrorCode)
             {
@@ -175,52 +175,6 @@ namespace LexicalAnalysis.LexicalAnalyzer
                 _inputIndexer += _lastCorrectLength;
             }
         }
-        private static void RecognizeNonWhitespace(ref string currentSubstring, ref int lastCorrectCode, ref int lastCorrectLength, string input, int inputIndexer)
-        {
-            int offset = 0;
-            int currentCode = int.MaxValue; //current lexical element to recognise
-            int currentLookaheadLength = 0;
-
-            while (inputIndexer + offset < input.Length &&
-                !IsWhitespace(input[inputIndexer + offset]) &&
-                currentCode != LexicalElementCodeDictionary.ErrorCode)
-            {
-                currentSubstring = input.Substring(inputIndexer, offset + 1);
-                currentCode = LexicalElementIdentifier.IdentifyLexicalElement(currentSubstring);
-                if (currentCode != LexicalElementCodeDictionary.ErrorCode)
-                {
-                    lastCorrectCode = currentCode;
-                    lastCorrectLength = offset + 1;
-                }
-
-                HandleConflict(ref currentCode, ref currentLookaheadLength, input, inputIndexer, lastCorrectCode, offset);
-
-                offset++;
-            }
-        }
-        private static void HandleConflict(ref int currentCode, ref int currentLookaheadLength, string input, int inputIndexer, int lastCorrectCode, int offset)
-        {
-            if (currentCode != LexicalElementCodeDictionary.ErrorCode)
-            {
-                return;
-            }
-
-            if (lastCorrectCode == LexicalElementCodeDictionary.GetCode("egész literál") && input[inputIndexer + offset] == ',')
-            {   // Conflict handling between integer and fractional literals
-                currentCode = int.MaxValue;
-            }
-            else if (LexicalElementCodeDictionary.IsOperator(lastCorrectCode) && input[inputIndexer + offset - 1] == '-')
-            {   // Conflict handling between the '-' operator and the following reserved words: "-tól", "-től", "-ig"
-                currentCode = int.MaxValue;
-                currentLookaheadLength = 2;
-            }
-            else if (currentLookaheadLength > 0)
-            {   // Checking and stepping allowed "lookahead" 
-                currentCode = int.MaxValue;
-                currentLookaheadLength--;
-            }
-        }
-
         #endregion
 
         private void AddNewLine()
@@ -274,7 +228,7 @@ namespace LexicalAnalysis.LexicalAnalyzer
         }
 
 
-        private static bool IsWhitespace(char c)
+        internal static bool IsWhitespace(char c)
             => c == ' ' || c == '\t' || c == '\n';
     }
 }
---
+++ LexicalAnalysis/LexicalAnalyzer/NonWhitespaceRecognizer.cs
@@ -0,0 +1,54 @@
+﻿using LexicalAnalysis.LexicalElementIdentification;
+
+namespace LexicalAnalysis.LexicalAnalyzer
+{
+    internal static class NonWhitespaceRecognizer
+    {
+        internal static void RecognizeNonWhitespace(ref string currentSubstring, ref int lastCorrectCode, ref int lastCorrectLength, string input, int inputIndexer)
+        {
+            int offset = 0;
+            int currentCode = int.MaxValue; //current lexical element to recognise
+            int currentLookaheadLength = 0;
+
+            while (inputIndexer + offset < input.Length &&
+                !LexicalAnalyzer.IsWhitespace(input[inputIndexer + offset]) &&
+                currentCode != LexicalElementCodeDictionary.ErrorCode)
+            {
+                currentSubstring = input.Substring(inputIndexer, offset + 1);
+                currentCode = LexicalElementIdentifier.IdentifyLexicalElement(currentSubstring);
+                if (currentCode != LexicalElementCodeDictionary.ErrorCode)
+                {
+                    lastCorrectCode = currentCode;
+                    lastCorrectLength = offset + 1;
+                }
+
+                HandleConflict(ref currentCode, ref currentLookaheadLength, input, inputIndexer, lastCorrectCode, offset);
+
+                offset++;
+            }
+        }
+        private static void HandleConflict(ref int currentCode, ref int currentLookaheadLength, string input, int inputIndexer, int lastCorrectCode, int offset)
+        {
+            if (currentCode != LexicalElementCodeDictionary.ErrorCode)
+            {
+                return;
+            }
+
+            if (lastCorrectCode == LexicalElementCodeDictionary.GetCode("egész literál") && input[inputIndexer + offset] == ',')
+            {   // Conflict handling between integer and fractional literals
+                currentCode = int.MaxValue;
+            }
+            else if (LexicalElementCodeDictionary.IsOperator(lastCorrectCode) && input[inputIndexer + offset - 1] == '-')
+            {   // Conflict handling between the '-' operator and the following reserved words: "-tól", "-től", "-ig"
+                currentCode = int.MaxValue;
+                currentLookaheadLength = 2;
+            }
+            else if (currentLookaheadLength > 0)
+            {   // Checking and stepping allowed "lookahead" 
+                currentCode = int.MaxValue;
+                currentLookaheadLength--;
+            }
+        }
+
+    }
+}
