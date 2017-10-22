215e0d31497661ab9f7ccdcbd3017433ae76531a
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-10-22 10:56:12 +0200

    Added test IfThenElseFi

--- Test/SyntaxAnalysisTests/SyntaxAnalysisTests/If.cs
+++ Test/SyntaxAnalysisTests/SyntaxAnalysisTests/If.cs
@@ -9,7 +9,7 @@ namespace SyntaxAnalysisTests
     public sealed class If
     {
         [Test]
-        public void If_Simple()
+        public void IfThenFi()
         {
             const string code = "program_kezd\r\n" +
                                 "ha 0 akkor\r\n" +
@@ -36,8 +36,50 @@ namespace SyntaxAnalysisTests
             állításIf.ExpectChildrenNames("ha", nameof(SA.NemTömbLétrehozóKifejezés), "akkor", "újsor", nameof(SA.Állítások), "elágazás_vége");
             állításIf.ExpectOneNemTömbLétrehozóKifejezésChildWithLiteralValue("egész literál", "0");
 
-            var állítások2 = állításIf.GetNonTerminalChildOfName(nameof(SA.Állítások));
-            állítások2.GetNonTerminalChildOfName(nameof(SA.Állítás)).ExpectChildrenNames("kilép");
+            állításIf.GetNonTerminalChildOfName(nameof(SA.Állítások))
+                .GetNonTerminalChildOfName(nameof(SA.Állítás))
+                    .ExpectChildrenNames("kilép");
+        }
+
+        [Test]
+        public void IfThenElseFi()
+        {
+            const string code = "program_kezd\r\n" +
+                                "ha 0 akkor\r\n" +
+                                "   kilép\r\n" +
+                                "különben\r\n" +
+                                "   kilép\r\n" +
+                                "elágazás_vége\r\n" +
+                                "program_vége";
+
+            ParseTree<Token> tree = TestHelper.Parse(code);
+
+            tree.ExpectLeaves(
+                "program_kezd", "újsor",
+                "ha", "egész literál", "akkor", "újsor",
+                "kilép", "újsor",
+                "különben", "újsor",
+                "kilép", "újsor",
+                "elágazás_vége", "újsor",
+                "program_vége");
+
+            var root = tree.Root;
+            TestHelper.CheckRoot(root, isOneStatementBody: true);
+
+            var állítások = root.GetNonTerminalChildOfName(nameof(SA.Állítások));
+            állítások.ExpectChildrenNames(nameof(SA.Állítás), "újsor");
+
+            var állításIf = állítások.GetNonTerminalChildOfName(nameof(SA.Állítás));
+            állításIf.ExpectChildrenNames("ha", nameof(SA.NemTömbLétrehozóKifejezés), "akkor", "újsor", nameof(SA.Állítások), "különben", "újsor", nameof(SA.Állítások), "elágazás_vége");
+            állításIf.ExpectOneNemTömbLétrehozóKifejezésChildWithLiteralValue("egész literál", "0");
+
+            állításIf.GetNthNonTerminalChildOfName(nameof(SA.Állítások), 1)
+                .GetNonTerminalChildOfName(nameof(SA.Állítás))
+                    .ExpectChildrenNames("kilép");
+
+            állításIf.GetNthNonTerminalChildOfName(nameof(SA.Állítások), 2)
+                .GetNonTerminalChildOfName(nameof(SA.Állítás))
+                .ExpectChildrenNames("kilép");
         }
     }
 }
