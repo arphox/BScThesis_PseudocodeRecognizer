48f4ba7f21a96c6b979a6d1630dab69145b273f3
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-11-02 19:02:44 +0100

    Finished test SimpleTheorem_Summation

--- Test/SyntaxAnalysisTests/SyntaxAnalysisTests/ComplexTests.cs
+++ Test/SyntaxAnalysisTests/SyntaxAnalysisTests/ComplexTests.cs
@@ -97,8 +97,82 @@ namespace SyntaxAnalysisTests
             var nemTömbLétrehozóKifejezés5 = állítás5.GetNonTerminalChildOfName(nameof(SA.NemTömbLétrehozóKifejezés));
             nemTömbLétrehozóKifejezés5.ExpectChildrenNames(nameof(SA.BinárisKifejezés));
 
+            // (6.)    egész temp = tömb[i]\r\n
+            var állítások6 = állítás5.GetNonTerminalChildOfName(nameof(SA.Állítások));
+            állítások6.ExpectChildrenNames(nameof(SA.Állítás), "újsor", nameof(SA.Állítások));
 
+            var állítás6 = állítások6.GetNonTerminalChildOfName(nameof(SA.Állítás));
+            állítás6.ExpectChildrenNames(nameof(SA.VáltozóDeklaráció));
 
+            var változóDeklaráció6 = állítás6.GetNonTerminalChildOfName(nameof(SA.VáltozóDeklaráció));
+            változóDeklaráció6.ExpectChildrenNames(nameof(SA.AlapTípus), "azonosító", "=", nameof(SA.NemTömbLétrehozóKifejezés));
+
+            változóDeklaráció6.GetNonTerminalChildOfName(nameof(SA.AlapTípus)).ExpectChildrenNames("egész");
+            változóDeklaráció6.GetTerminalChildOfName("azonosító").ExpectIdentifierNameOf("temp");
+
+            var nemTömbLétrehozóKifejezés6 = változóDeklaráció6.GetNonTerminalChildOfName(nameof(SA.NemTömbLétrehozóKifejezés));
+            nemTömbLétrehozóKifejezés6.ExpectChildrenNames(nameof(SA.Operandus));
+
+            var operandus6a = nemTömbLétrehozóKifejezés6.GetNonTerminalChildOfName(nameof(SA.Operandus));
+            operandus6a.ExpectChildrenNames("azonosító", "[", nameof(SA.Operandus), "]");
+
+            var operandus6b = operandus6a.GetNonTerminalChildOfName(nameof(SA.Operandus));
+            operandus6b.ExpectChildrenNames("azonosító");
+            operandus6b.GetTerminalChildOfName("azonosító").ExpectIdentifierNameOf("i");
+
+            // (7.)    összeg = összeg + temp\r\n
+            var állítások7 = állítások6.GetNonTerminalChildOfName(nameof(SA.Állítások));
+            állítások7.ExpectChildrenNames(nameof(SA.Állítás), "újsor", nameof(SA.Állítások));
+
+            var állítás7 = állítások7.GetNonTerminalChildOfName(nameof(SA.Állítás));
+            állítás7.ExpectChildrenNames(nameof(SA.Értékadás));
+
+            var értékadás7 = állítás7.GetNonTerminalChildOfName(nameof(SA.Értékadás));
+            értékadás7.ExpectChildrenNames("azonosító", "=", nameof(SA.NemTömbLétrehozóKifejezés));
+
+            var nemTömbLétrehozóKifejezés7 = értékadás7.GetNonTerminalChildOfName(nameof(SA.NemTömbLétrehozóKifejezés));
+            nemTömbLétrehozóKifejezés7.ExpectChildrenNames(nameof(SA.BinárisKifejezés));
+
+            var bináriskifejezés7 = nemTömbLétrehozóKifejezés7.GetNonTerminalChildOfName(nameof(SA.BinárisKifejezés));
+            bináriskifejezés7.ExpectChildrenNames(nameof(SA.Operandus), nameof(SA.BinárisOperátor), nameof(SA.Operandus));
+
+            var operandus7a = bináriskifejezés7.Children[0];
+            operandus7a.ExpectChildrenNames("azonosító");
+            operandus7a.GetTerminalChildOfName("azonosító").ExpectIdentifierNameOf("összeg");
+
+            var binárisOperátor7 = bináriskifejezés7.GetNonTerminalChildOfName(nameof(SA.BinárisOperátor));
+            binárisOperátor7.ExpectChildrenNames("+");
+
+            var operandus7b = bináriskifejezés7.Children[2];
+            operandus7b.ExpectChildrenNames("azonosító");
+            operandus7b.GetTerminalChildOfName("azonosító").ExpectIdentifierNameOf("temp");
+
+            // (8.)    i = i + 1\r\n
+            var állítások8 = állítások7.GetNonTerminalChildOfName(nameof(SA.Állítások));
+            állítások8.ExpectChildrenNames(nameof(SA.Állítás), "újsor");
+
+            var állítás8 = állítások8.GetNonTerminalChildOfName(nameof(SA.Állítás));
+            állítás8.ExpectChildrenNames(nameof(SA.Értékadás));
+
+            var értékadás8 = állítás8.GetNonTerminalChildOfName(nameof(SA.Értékadás));
+            értékadás8.ExpectChildrenNames("azonosító", "=", nameof(SA.NemTömbLétrehozóKifejezés));
+
+            var nemTömbLétrehozóKifejezés8 = értékadás8.GetNonTerminalChildOfName(nameof(SA.NemTömbLétrehozóKifejezés));
+            nemTömbLétrehozóKifejezés8.ExpectChildrenNames(nameof(SA.BinárisKifejezés));
+
+            var bináriskifejezés8 = nemTömbLétrehozóKifejezés8.GetNonTerminalChildOfName(nameof(SA.BinárisKifejezés));
+            bináriskifejezés8.ExpectChildrenNames(nameof(SA.Operandus), nameof(SA.BinárisOperátor), nameof(SA.Operandus));
+
+            var operandus8a = bináriskifejezés8.Children[0];
+            operandus8a.ExpectChildrenNames("azonosító");
+            operandus8a.GetTerminalChildOfName("azonosító").ExpectIdentifierNameOf("i");
+
+            var binárisOperátor8 = bináriskifejezés8.GetNonTerminalChildOfName(nameof(SA.BinárisOperátor));
+            binárisOperátor8.ExpectChildrenNames("+");
+
+            var operandus8b = bináriskifejezés8.Children[2];
+            operandus8b.ExpectChildrenNames("egész literál");
+            operandus8b.GetTerminalChildOfName("egész literál").ExpectLiteralValueOf("1");
 
             // (10.) kiír összeg\r\n
             var állítások10 = állítások5.GetNonTerminalChildOfName(nameof(SA.Állítások));
