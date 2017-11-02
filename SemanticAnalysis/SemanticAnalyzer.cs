1961c0f6c9c36919896ddc508375b601561d56bd
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-11-02 20:44:11 +0100

    Added project SemanticAnalyzer

---
+++ SemanticAnalysis/SemanticAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using LexicalAnalysis.Tokens;
+//using SyntaxAnalysis.Tree;
+
+//namespace SemanticAnalysis
+//{
+//    public sealed class SemanticAnalyzer
+//    {
+//        public SemanticAnalyzer(ParseTree<Token> parseTree)
+//        {
+            
+//        }
+//    }
+//}
