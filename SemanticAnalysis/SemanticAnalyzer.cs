830c182fb9d95fb2cbab8821c1bc56b0c619c4b7
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-11-03 10:11:34 +0100

    Added SemanticAnalyzerException and ctor for SemA

--- SemanticAnalysis/SemanticAnalyzer.cs
+++ SemanticAnalysis/SemanticAnalyzer.cs
@@ -1,18 +1,23 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using LexicalAnalysis.Tokens;
-//using SyntaxAnalysis.Tree;
+﻿using System;
+using LexicalAnalysis.Tokens;
+using SyntaxAnalysis.Analyzer;
+using SyntaxAnalysis.Tree;
 
-//namespace SemanticAnalysis
-//{
-//    public sealed class SemanticAnalyzer
-//    {
-//        public SemanticAnalyzer(ParseTree<Token> parseTree)
-//        {
-            
-//        }
-//    }
-//}
+namespace SemanticAnalysis
+{
+    public sealed class SemanticAnalyzer
+    {
+        private readonly ParseTree<Token> _parseTree;
+
+        public SemanticAnalyzer(SyntaxAnalyzerResult parserResult)
+        {
+            if (parserResult == null)
+                throw new ArgumentNullException(nameof(parserResult));
+
+            if (!parserResult.IsSuccessful)
+                throw new SemanticAnalyzerException("The semantic analyzer only starts if there are no syntax errors.");
+
+            _parseTree = parserResult.ParseTree;
+        }
+    }
+}
---
+++ SemanticAnalysis/SemanticAnalyzerException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SemanticAnalysis
+{
+    public sealed class SemanticAnalyzerException : ApplicationException
+    {
+        public SemanticAnalyzerException(string message)
+            : base(message)
+        { }
+        
+        public override string Message => base.Message + ", " + ToString();
+    }
+}
