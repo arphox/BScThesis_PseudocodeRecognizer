a5edaa2b2a182cf9a3cda575a6f641c8c5da8998
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-11-05 22:04:13 +0100

    Added test case Complex1

--- Test/SemanticAnalysisTests/PositiveTests/ComplexCorrectCodes.cs
+++ Test/SemanticAnalysisTests/PositiveTests/ComplexCorrectCodes.cs
@@ -9,6 +9,19 @@ namespace SemanticAnalysisTests.PositiveTests
     {
         public static IEnumerable CodeProvider() => SimpleCorrectCodes.GetCodeProvider(typeof(ComplexCorrectCodes));
 
-        const string A = "program_kezd\r\n" + "kilép\r\n" + "program_vége";
+        public const string Complex1 =
+            "program_kezd\r\n" +
+            "szöveg beSzoveg = \"\"\r\n" +
+            "beolvas beSzoveg\r\n" +
+            "egész a = szövegből_egészbe(beSzoveg)\r\n" +
+            "a = a + 1\r\n" +
+            "ciklus_amíg a < 100\r\n" +
+            "   egész m = a mod 2\r\n" +
+            "   ha m == 0 akkor\r\n" +
+            "       szöveg temp = egészből_szövegbe(m)\r\n" +
+            "       kiír temp\r\n" +
+            "   elágazás_vége\r\n" +
+            "ciklus_vége\r\n" +
+            "program_vége";
     }
 }
--- Test/SemanticAnalysisTests/PositiveTests/SimpleCorrectCodes.cs
+++ Test/SemanticAnalysisTests/PositiveTests/SimpleCorrectCodes.cs
@@ -4,74 +4,71 @@ using System.Linq;
 using System.Reflection;
 using NUnit.Framework;
 
-// ReSharper disable UnusedMember.Local
-// ReSharper disable InconsistentNaming
-
 namespace SemanticAnalysisTests.PositiveTests
 {
     public static class SimpleCorrectCodes
     {
         public static IEnumerable GetCodeProvider(Type type)
         {
-            return type.GetFields(BindingFlags.NonPublic | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+            return type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
                 .Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(string))
                 .Select(x => new TestCaseData((string)x.GetRawConstantValue()).SetName(x.Name));
         }
 
         public static IEnumerable CodeProvider() => GetCodeProvider(typeof(SimpleCorrectCodes));
 
-        const string NoSemanticAnalysisNeeded_OneLine = "program_kezd\r\n" + "kilép\r\n" + "program_vége";
-        const string NoSemanticAnalysisNeeded_TwoLines = "program_kezd\r\n" + "kilép\r\n" + "kilép\r\n" + "program_vége";
+        public const string NoSemanticAnalysisNeeded_OneLine = "program_kezd\r\n" + "kilép\r\n" + "program_vége";
+        public const string NoSemanticAnalysisNeeded_TwoLines = "program_kezd\r\n" + "kilép\r\n" + "kilép\r\n" + "program_vége";
 
         // Declaration
-        const string NonArrayDeclaration1_Egesz = "program_kezd\r\n" + "egész a = 2\r\n" + "program_vége";
-        const string NonArrayDeclaration1_Tort = "program_kezd\r\n" + "tört a = 2,3\r\n" + "program_vége";
-        const string NonArrayDeclaration1_Szoveg = "program_kezd\r\n" + "szöveg a = \"körte\"\r\n" + "program_vége";
-        const string NonArrayDeclaration1_Logikai = "program_kezd\r\n" + "logikai a = hamis\r\n" + "program_vége";
-        const string NonArrayDeclaration2_Egesz = "program_kezd\r\n" + "egész a = törtből_egészbe(2,2)\r\n" + "program_vége";
-        const string NonArrayDeclaration2_Tort = "program_kezd\r\n" + "tört a = egészből_törtbe(2)\r\n" + "program_vége";
-        const string NonArrayDeclaration2_Szoveg = "program_kezd\r\n" + "szöveg a = logikaiból_szövegbe(hamis)\r\n" + "program_vége";
-        const string NonArrayDeclaration2_Logikai = "program_kezd\r\n" + "logikai a = szövegből_logikaiba(\"hamis\")\r\n" + "program_vége";
-        const string ArrayDeclaration1_Egesz = "program_kezd\r\n" + "egész[] tömb = létrehoz[10]\r\n" + "program_vége";
-        const string ArrayDeclaration1_Tort = "program_kezd\r\n" + "tört[] tömb = létrehoz[10]\r\n" + "program_vége";
-        const string ArrayDeclaration1_Szoveg = "program_kezd\r\n" + "szöveg[] tömb = létrehoz[10]\r\n" + "program_vége";
-        const string ArrayDeclaration1_Logikai = "program_kezd\r\n" + "logikai[] tömb = létrehoz[10]\r\n" + "program_vége";
-        const string ArrayDeclaration2_Egesz = "program_kezd\r\n" + "egész[] tömb = létrehoz[10]\r\n" + "egész[] tömb2 = tömb\r\n" + "program_vége";
-        const string ArrayDeclaration2_Tort = "program_kezd\r\n" + "tört[] tömb = létrehoz[10]\r\n" + "tört[] tömb2 = tömb\r\n" + "program_vége";
-        const string ArrayDeclaration2_Szoveg = "program_kezd\r\n" + "szöveg[] tömb = létrehoz[10]\r\n" + "szöveg[] tömb2 = tömb\r\n" + "program_vége";
-        const string ArrayDeclaration2_Logikai = "program_kezd\r\n" + "logikai[] tömb = létrehoz[10]\r\n" + "logikai[] tömb2 = tömb\r\n" + "program_vége";
+        public const string NonArrayDeclaration1_Egesz = "program_kezd\r\n" + "egész a = 2\r\n" + "program_vége";
+        public const string NonArrayDeclaration1_Tort = "program_kezd\r\n" + "tört a = 2,3\r\n" + "program_vége";
+        public const string NonArrayDeclaration1_Szoveg = "program_kezd\r\n" + "szöveg a = \"körte\"\r\n" + "program_vége";
+        public const string NonArrayDeclaration1_Logikai = "program_kezd\r\n" + "logikai a = hamis\r\n" + "program_vége";
+        public const string NonArrayDeclaration2_Egesz = "program_kezd\r\n" + "egész a = törtből_egészbe(2,2)\r\n" + "program_vége";
+        public const string NonArrayDeclaration2_Tort = "program_kezd\r\n" + "tört a = egészből_törtbe(2)\r\n" + "program_vége";
+        public const string NonArrayDeclaration2_Szoveg = "program_kezd\r\n" + "szöveg a = logikaiból_szövegbe(hamis)\r\n" + "program_vége";
+        public const string NonArrayDeclaration2_Logikai = "program_kezd\r\n" + "logikai a = szövegből_logikaiba(\"hamis\")\r\n" + "program_vége";
+        public const string ArrayDeclaration1_Egesz = "program_kezd\r\n" + "egész[] tömb = létrehoz[10]\r\n" + "program_vége";
+        public const string ArrayDeclaration1_Tort = "program_kezd\r\n" + "tört[] tömb = létrehoz[10]\r\n" + "program_vége";
+        public const string ArrayDeclaration1_Szoveg = "program_kezd\r\n" + "szöveg[] tömb = létrehoz[10]\r\n" + "program_vége";
+        public const string ArrayDeclaration1_Logikai = "program_kezd\r\n" + "logikai[] tömb = létrehoz[10]\r\n" + "program_vége";
+        public const string ArrayDeclaration2_Egesz = "program_kezd\r\n" + "egész[] tömb = létrehoz[10]\r\n" + "egész[] tömb2 = tömb\r\n" + "program_vége";
+        public const string ArrayDeclaration2_Tort = "program_kezd\r\n" + "tört[] tömb = létrehoz[10]\r\n" + "tört[] tömb2 = tömb\r\n" + "program_vége";
+        public const string ArrayDeclaration2_Szoveg = "program_kezd\r\n" + "szöveg[] tömb = létrehoz[10]\r\n" + "szöveg[] tömb2 = tömb\r\n" + "program_vége";
+        public const string ArrayDeclaration2_Logikai = "program_kezd\r\n" + "logikai[] tömb = létrehoz[10]\r\n" + "logikai[] tömb2 = tömb\r\n" + "program_vége";
 
         // Assignment
-        const string Assignment1_Egesz = "program_kezd\r\n" + "egész a = 2\r\n" + "a = -10\r\n" + "program_vége";
-        const string Assignment1_Tort = "program_kezd\r\n" + "tört a = 2,4\r\n" + "a = 7,45\r\n" + "program_vége";
-        const string Assignment1_Szoveg = "program_kezd\r\n" + "szöveg a = \"kukor\" . \"ica\"\r\n" + "a = \"vége\"\r\n" + "program_vége";
-        const string Assignment1_Logikai = "program_kezd\r\n" + "logikai a = igaz vagy hamis\r\n" + "a = hamis\r\n" + "program_vége";
-        const string Assignment2_Egesz = "program_kezd\r\n" + "egész[] tömb = létrehoz[3]\r\n" + "tömb = létrehoz[5]\r\n" + "program_vége";
-        const string Assignment2_Tort = "program_kezd\r\n" + "tört[] tömb = létrehoz[3]\r\n" + "tömb = létrehoz[5]\r\n" + "program_vége";
-        const string Assignment2_Szoveg = "program_kezd\r\n" + "szöveg[] tömb = létrehoz[3]\r\n" + "tömb = létrehoz[5]\r\n" + "program_vége";
-        const string Assignment2_Logikai = "program_kezd\r\n" + "logikai[] tömb = létrehoz[3]\r\n" + "tömb = létrehoz[5]\r\n" + "program_vége";
-        const string Assignment3_Egesz = "program_kezd\r\n" + "egész a = 2\r\n" + "a = törtből_egészbe(1,23)\r\n" + "program_vége";
-        const string Assignment3_Tort = "program_kezd\r\n" + "tört a = 2,4\r\n" + "a = egészből_törtbe(1)\r\n" + "program_vége";
-        const string Assignment3_Szoveg = "program_kezd\r\n" + "szöveg a = \"barack\"\r\n" + "a = törtből_szövegbe(3,4)\r\n" + "program_vége";
-        const string Assignment3_Logikai = "program_kezd\r\n" + "logikai a = ! igaz\r\n" + "a = szövegből_logikaiba(\"asd\")\r\n" + "program_vége";
-        const string Assignment4_Egesz = "program_kezd\r\n" + "egész[] tömb = létrehoz[10]\r\n" + "tömb[0] = 3 + 4\r\n" + "program_vége";
-        const string Assignment4_Tort = "program_kezd\r\n" + "tört[] tömb = létrehoz[10]\r\n" + "tömb[0] = 78,0\r\n" + "program_vége";
-        const string Assignment4_Szoveg = "program_kezd\r\n" + "szöveg[] tömb = létrehoz[10]\r\n" + "tömb[0] = \"szilva\"\r\n" + "program_vége";
-        const string Assignment4_Logikai = "program_kezd\r\n" + "logikai[] tömb = létrehoz[10]\r\n" + "tömb[0] = igaz\r\n" + "program_vége";
+        public const string Assignment1_Egesz = "program_kezd\r\n" + "egész a = 2\r\n" + "a = -10\r\n" + "program_vége";
+        public const string Assignment1_Tort = "program_kezd\r\n" + "tört a = 2,4\r\n" + "a = 7,45\r\n" + "program_vége";
+        public const string Assignment1_Szoveg = "program_kezd\r\n" + "szöveg a = \"kukor\" . \"ica\"\r\n" + "a = \"vége\"\r\n" + "program_vége";
+        public const string Assignment1_Logikai = "program_kezd\r\n" + "logikai a = igaz vagy hamis\r\n" + "a = hamis\r\n" + "program_vége";
+        public const string Assignment2_Egesz = "program_kezd\r\n" + "egész[] tömb = létrehoz[3]\r\n" + "tömb = létrehoz[5]\r\n" + "program_vége";
+        public const string Assignment2_Tort = "program_kezd\r\n" + "tört[] tömb = létrehoz[3]\r\n" + "tömb = létrehoz[5]\r\n" + "program_vége";
+        public const string Assignment2_Szoveg = "program_kezd\r\n" + "szöveg[] tömb = létrehoz[3]\r\n" + "tömb = létrehoz[5]\r\n" + "program_vége";
+        public const string Assignment2_Logikai = "program_kezd\r\n" + "logikai[] tömb = létrehoz[3]\r\n" + "tömb = létrehoz[5]\r\n" + "program_vége";
+        public const string Assignment3_Egesz = "program_kezd\r\n" + "egész a = 2\r\n" + "a = törtből_egészbe(1,23)\r\n" + "program_vége";
+        public const string Assignment3_Tort = "program_kezd\r\n" + "tört a = 2,4\r\n" + "a = egészből_törtbe(1)\r\n" + "program_vége";
+        public const string Assignment3_Szoveg = "program_kezd\r\n" + "szöveg a = \"barack\"\r\n" + "a = törtből_szövegbe(3,4)\r\n" + "program_vége";
+        public const string Assignment3_Logikai = "program_kezd\r\n" + "logikai a = ! igaz\r\n" + "a = szövegből_logikaiba(\"asd\")\r\n" + "program_vége";
+        public const string Assignment4_Egesz = "program_kezd\r\n" + "egész[] tömb = létrehoz[10]\r\n" + "tömb[0] = 3 + 4\r\n" + "program_vége";
+        public const string Assignment4_Tort = "program_kezd\r\n" + "tört[] tömb = létrehoz[10]\r\n" + "tömb[0] = 78,0\r\n" + "program_vége";
+        public const string Assignment4_Szoveg = "program_kezd\r\n" + "szöveg[] tömb = létrehoz[10]\r\n" + "tömb[0] = \"szilva\"\r\n" + "program_vége";
+        public const string Assignment4_Logikai = "program_kezd\r\n" + "logikai[] tömb = létrehoz[10]\r\n" + "tömb[0] = igaz\r\n" + "program_vége";
 
         // While
-        const string While1 = "program_kezd\r\n" + "egész i = 0\r\n" + "ciklus_amíg i < 10\r\n" + "i = - i + 2\r\n" + "ciklus_vége\r\n" + "program_vége";
-        const string While2 = "program_kezd\r\n" + "egész i = 0\r\n" + "ciklus_amíg i < 10\r\n" + "i = i + 1\r\n" + "i = i + 1\r\n" + "ciklus_vége\r\n" + "program_vége";
+        public const string While1 = "program_kezd\r\n" + "egész i = 0\r\n" + "ciklus_amíg i < 10\r\n" + "i = - i + 2\r\n" + "ciklus_vége\r\n" + "program_vége";
+        public const string While2 = "program_kezd\r\n" + "egész i = 0\r\n" + "ciklus_amíg i < 10\r\n" + "i = i + 1\r\n" + "i = i + 1\r\n" + "ciklus_vége\r\n" + "program_vége";
 
         // If
-        const string If1 = "program_kezd\r\n" + "ha hamis akkor\r\n" + "kilép\r\n" + "elágazás_vége\r\n" + "program_vége";
-        const string If2 = "program_kezd\r\n" + "ha hamis akkor\r\n" + "kilép\r\n" + "különben\r\n" + "kilép\r\n" + "elágazás_vége\r\n" + "program_vége";
+        public const string If1 = "program_kezd\r\n" + "ha hamis akkor\r\n" + "kilép\r\n" + "elágazás_vége\r\n" + "program_vége";
+        public const string If2 = "program_kezd\r\n" + "ha hamis akkor\r\n" + "kilép\r\n" + "különben\r\n" + "kilép\r\n" + "elágazás_vége\r\n" + "program_vége";
 
         // IoParancs
-        const string IoParancs_Beolvas = "program_kezd\r\n" + "szöveg a = \"\"\r\n" + "beolvas a\r\n" + "program_vége";
-        const string IoParancs_Kiir = "program_kezd\r\n" + "szöveg a = \"alma\"\r\n" + "kiír a\r\n" + "program_vége";
+        public const string IoParancs_Beolvas = "program_kezd\r\n" + "szöveg a = \"\"\r\n" + "beolvas a\r\n" + "program_vége";
+        public const string IoParancs_Kiir = "program_kezd\r\n" + "szöveg a = \"alma\"\r\n" + "kiír a\r\n" + "program_vége";
 
         // Operandus
-        const string Operandus1 = "program_kezd\r\n" + "egész[] tömb = létrehoz[3]\r\n" + "egész a = tömb[4]\r\n" + "program_vége";
+        public const string Operandus1 = "program_kezd\r\n" + "egész[] tömb = létrehoz[3]\r\n" + "egész a = tömb[4]\r\n" + "program_vége";
     }
 }
