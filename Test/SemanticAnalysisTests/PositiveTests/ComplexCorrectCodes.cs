d61290e32aaad6056303d46f09ceb507397e33c7
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-11-05 22:21:08 +0100

    Added Complex 2-3-4 test cases

--- Test/SemanticAnalysisTests/PositiveTests/ComplexCorrectCodes.cs
+++ Test/SemanticAnalysisTests/PositiveTests/ComplexCorrectCodes.cs
@@ -20,5 +20,37 @@ namespace SemanticAnalysisTests.PositiveTests
             "   elágazás_vége\r\n" +
             "ciklus_vége\r\n" +
             "program_vége";
+
+        public const string Complex2 =
+            "program_kezd\r\n" +
+            "egész[] számok = létrehoz[10]\r\n" +
+            "egész i = 0\r\n" +
+            "ciklus_amíg i < 10\r\n" +
+            "   számok[i] = i + 1\r\n" +
+            "ciklus_vége\r\n" +
+            "i = 0\r\n" +
+            "ciklus_amíg i < 10\r\n" +
+            "   számok[i] = számok[i] * 2\r\n" +
+            "ciklus_vége\r\n" +
+            "program_vége";
+
+        public const string Complex3 =
+            "program_kezd\r\n" +
+            "egész e = 2\r\n" +
+            "tört t = egészből_törtbe(e)\r\n" +
+            "szöveg sz = törtből_szövegbe(t)\r\n" +
+            "logikai l = szövegből_logikaiba(sz)\r\n" +
+            "egész final = logikaiból_egészbe(l)\r\n" +
+            "szöveg finalSzöveg = egészből_szövegbe(final)\r\n" +
+            "kiír finalSzöveg\r\n" +
+            "program_vége";
+
+        public const string Complex4 =
+            "program_kezd\r\n" +
+            "egész e = 2\r\n" +
+            "szöveg eSz = egészből_szövegbe(e)\r\n" +
+            "szöveg f = \"Az e változó értéke: \" . eSz\r\n" +
+            "kiír f\r\n" +
+            "program_vége";
     }
 }
