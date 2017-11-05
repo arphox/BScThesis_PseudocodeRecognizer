4c84884e802de29d12c01d574c74ccde2173f949
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-11-05 11:10:57 +0100

    Added tests ArrayDeclaration2

--- Test/SemanticAnalysisTests/DeclarationTests/ArrayDeclarationTests.cs
+++ Test/SemanticAnalysisTests/DeclarationTests/ArrayDeclarationTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using LexicalAnalysis.SymbolTableManagement;
+﻿using LexicalAnalysis.SymbolTableManagement;
 using NUnit.Framework;
 using SemanticAnalysis.Exceptions;
 
@@ -27,6 +26,41 @@ namespace SemanticAnalysisTests.DeclarationTests
 
             SemanticAnalysisException ex = TestHelper.DoSemanticAnalysisWithExceptionSwallowing(code);
             TestHelper.ExpectAnotherTypeExpectedException(ex, SingleEntryType.Egesz.ToString(), SingleEntryType.Logikai.ToString());
+        }
+
+        [Test]
+        public void ArrayDeclaration2()
+        {
+            const string code = "program_kezd\r\n" +
+                                "egész[] tömb = létrehoz[10]\r\n" +
+                                "egész[] tömb2 = tömb\r\n" +
+                                "program_vége";
+
+            TestHelper.DoSemanticAnalysis(code);
+        }
+
+        [Test]
+        public void ArrayDeclaration2_Negative1()
+        {
+            const string code = "program_kezd\r\n" +
+                                "tört[] tömb = létrehoz[10]\r\n" +
+                                "egész[] tömb2 = tömb\r\n" +
+                                "program_vége";
+
+            SemanticAnalysisException ex = TestHelper.DoSemanticAnalysisWithExceptionSwallowing(code);
+            TestHelper.ExpectAnotherTypeExpectedException(ex, SingleEntryType.EgeszTomb.ToString(), SingleEntryType.TortTomb.ToString());
+        }
+
+        [Test]
+        public void ArrayDeclaration2_Negative2()
+        {
+            const string code = "program_kezd\r\n" +
+                                "egész[] tömb = létrehoz[10]\r\n" +
+                                "tört[] tömb2 = tömb\r\n" +
+                                "program_vége";
+
+            SemanticAnalysisException ex = TestHelper.DoSemanticAnalysisWithExceptionSwallowing(code);
+            TestHelper.ExpectAnotherTypeExpectedException(ex, SingleEntryType.TortTomb.ToString(), SingleEntryType.EgeszTomb.ToString());
         }
     }
 }
