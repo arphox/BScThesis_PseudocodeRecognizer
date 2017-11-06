2a28c6409878e1d92eba540a20e442fcf4165a9c
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-11-06 09:14:37 +0100

    Added the Compiler project

---
+++ Compiler/Compiler.cs
@@ -0,0 +1,24 @@
+﻿using LexicalAnalysis.Analyzer;
+using SemanticAnalysis;
+using SyntaxAnalysis.Analyzer;
+
+namespace Compiler
+{
+    public sealed class Compiler
+    {
+        public void Compile(string code)
+        {
+            // Lexical analysis
+            LexicalAnalyzer lex = new LexicalAnalyzer(code);
+            LexicalAnalyzerResult lexResult = lex.Start();
+
+            // Syntax analysis
+            SyntaxAnalyzer syn = new SyntaxAnalyzer(lexResult);
+            SyntaxAnalyzerResult synResult = syn.Start();
+
+            // Semantical analysis
+            SemanticAnalyzer sem = new SemanticAnalyzer(synResult, lexResult.SymbolTable);
+            sem.Start();
+        }
+    }
+}
