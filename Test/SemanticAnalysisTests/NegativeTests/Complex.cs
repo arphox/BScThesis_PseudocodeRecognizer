c65ef53b95a940244a0633c367817153aeff8588
Károly Ozsvárt <ozsvart.karoly@gmail.com>
2017-11-06 07:32:18 +0100

    Added Complex2_Negative

---
+++ Test/SemanticAnalysisTests/NegativeTests/Complex.cs
@@ -0,0 +1,51 @@
+﻿using LexicalAnalysis.SymbolTableManagement;
+using NUnit.Framework;
+using SemanticAnalysis.Exceptions;
+
+namespace SemanticAnalysisTests.NegativeTests
+{
+    [TestFixture]
+    public sealed class Complex
+    {
+        [Test]
+        public void Complex1_Negative()
+        {
+            const string code = "program_kezd\r\n" +
+                                "szöveg beSzoveg = \"\"\r\n" +
+                                "beolvas beSzoveg\r\n" +
+                                "egész a = szövegből_egészbe(beSzoveg)\r\n" +
+                                "a = a + 1\r\n" +
+                                "ciklus_amíg a < 100\r\n" +
+                                "   egész m = a mod 2\r\n" +
+                                "   ha m = 0 akkor\r\n" +
+                                "       szöveg temp = egészből_szövegbe(m)\r\n" +
+                                "       kiír temp\r\n" +
+                                "   elágazás_vége\r\n" +
+                                "ciklus_vége\r\n" +
+                                "program_vége";
+
+            SemanticAnalysisException e = TestHelper.DoSemanticAnalysisWithExceptionSwallowing(code);
+            Assert.That(e.Message.Contains("Unexpected binary operator"));
+            Assert.That(e.Line, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void Complex2_Negative()
+        {
+            const string code = "program_kezd\r\n" +
+                                "egész[] számok = létrehoz[10]\r\n" +
+                                "egész i = 0\r\n" +
+                                "ciklus_amíg i < 10\r\n" +
+                                "   számok[i] = i . 1\r\n" +
+                                "ciklus_vége\r\n" +
+                                "i = 0\r\n" +
+                                "ciklus_amíg i < 10\r\n" +
+                                "   számok[i] = számok[i] és 2\r\n" + // <- this should be indicated, too.
+                                "ciklus_vége\r\n" +
+                                "program_vége";
+
+            SemanticAnalysisException e = TestHelper.DoSemanticAnalysisWithExceptionSwallowing(code);
+            TestHelper.ExpectAnotherTypeExpectedException(e, SingleEntryType.Szoveg.ToString(), SingleEntryType.Egesz.ToString() ,5);
+        }
+    }
+}
